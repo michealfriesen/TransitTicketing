e1ab44a22c378f3171e11ad5304da8d6049d69b1
Jacky Tang <jickay@gmail.com>
2018-10-22 12:55:48 -0600

    Push project folders and files
    Not sure how the structure works yet

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TransitTicketing
+namespace WpfApp1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,8 +23,6 @@ namespace TransitTicketing
         public MainWindow()
         {
             InitializeComponent();
-
-
         }
     }
 }
